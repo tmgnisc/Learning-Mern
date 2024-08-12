60500ba57432dfe6c554bd7a92fa6bbae9673cef
Nischal <tamangnischal2018@gmail.com>
2024-08-12 20:11:17 +0545

    created server using express js

--- server/server.js
+++ server/server.js
@@ -4,7 +4,12 @@ app.get("/", (req, res) => {
 
     res.status(200).send("welcome to nischal learning")
 })  //default root page or home page, first argument homepage and second argument should be response
+app.get("/register", (req, res) => {
+
+    res.status(200).send("welcome to register page ")
+}) 
 const port = 5000
+
 app.listen(port ,() =>{
     console.log(`server is running in port ${port}`)
 })
