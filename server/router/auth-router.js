50cdaa29175a7ed39063433e7c5f1081103abd4a
Nischal <tamangnischal2018@gmail.com>
2024-08-13 20:25:52 +0545

    integrating the router in main server file

--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,2 +1,12 @@
 const express = require("express")
 const  router = express.Router()
+
+// app.get("/", (req, res) =>{
+//     res.send("welcome")
+// })
+
+router.get("/", (req, res) =>{
+    res.status(200).send("welcome using router")
+})
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,5 +1,9 @@
 const express = require("express")
 const app = express()
+
+const router = require('./router/auth-router')
+app.use("/api/auth", router)
+
 app.get("/", (req, res) => {
 
     res.status(200).send("welcome to nischal learning")
