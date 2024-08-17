33e7a139cd69a7edef82a9e84e465051961d85bf
Nischal <tamangnischal2018@gmail.com>
2024-08-17 16:10:20 +0545

    connected to backend

--- server/server.js
+++ server/server.js
@@ -2,12 +2,14 @@ const express = require("express")
 const app = express()
  
 const router = require('./router/auth-router')
+const connectDb = require('./utils/db')
 app.use(express.json())
 app.use("/api/auth", router)
 
 
 const port = 5000
-
-app.listen(port ,() =>{
-    console.log(`server is running in port ${port}`)
+connectDb().then(() => {
+    app.listen(port ,() =>{
+        console.log(`server is running in port ${port}`)
+    })
 })
--- server/utils/db.js
+++ server/utils/db.js
@@ -1,3 +1,14 @@
 const mongoose = require('mongoose')
 const URI = "mongodb://127.0.0.1:27017/mern_admin"
-mongoose.connect(URI)
+// mongoose.connect(URI)
+const connectDb = async() => {
+    try {
+        await mongoose.connect(URI)
+        console.log("connection successful to db")
+    } catch (error) {
+        console.error("database connection failed")
+        process.exit(0)
+    }
+}
+
+module.exports = connectDb
