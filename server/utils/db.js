78314cc04cc7aec9b5ec37baeb6c939f57e5a1df
Nischal <tamangnischal2018@gmail.com>
2024-08-17 18:33:16 +0545

    creating the user schema

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,14 +1,17 @@
-const mongoose = require('mongoose')
-const URI = "mongodb://127.0.0.1:27017/mern_admin"
-// mongoose.connect(URI)
-const connectDb = async() => {
-    try {
-        await mongoose.connect(URI)
-        console.log("connection successful to db")
-    } catch (error) {
-        console.error("database connection failed")
-        process.exit(0)
-    }
-}
+const mongoose = require("mongoose");
+// const URI = "mongodb://127.0.0.1:27017/mern_admin"
 
-module.exports = connectDb
+const URI = process.env.MONGODB_URI
+
+// mongoose.connect(URI)
+const connectDb = async () => {
+  try {
+    await mongoose.connect(URI);
+    console.log("connection successful to db");
+  } catch (error) {
+    console.error("database connection failed");
+    process.exit(0);
+  }
+};
+
+module.exports = connectDb;
