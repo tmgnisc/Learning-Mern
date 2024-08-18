461cda3c9204ab66e9291059575e67076612190c
Nischal <tamangnischal2018@gmail.com>
2024-08-18 19:53:11 +0545

    implementing bcrypt

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,25 +1,33 @@
-const User = require('../models/user-model')
-const home = async (req, res) =>{
-    try {
-        res.status(200).send("this is home page using controller")
-    } catch (error) {
-        console.log(error)
-        
-    }
-}
+const User = require("../models/user-model");
+const bcrypt = require("bcryptjs");
+const home = async (req, res) => {
+  try {
+    res.status(200).send("this is home page using controller");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-const register = async (req, res) =>{
-    try {
-        // console.log(req.body)
-        const {username, email, phone, password} = req.body;
-        const userExists = await User.findOne({email})
-        if(userExists){
-            return res.status(400).json({msg:"email already exists"})
-        }
-const userCreated = await User.create({username, email, phone, password})
-        res.status(200).json({message: userCreated})
-    } catch (error) {
-        res.status(400).send("this is error ")
+const register = async (req, res) => {
+  try {
+    // console.log(req.body)
+    const { username, email, phone, password } = req.body;
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ msg: "email already exists" });
     }
-}
-module.exports = {home, register}
+    //hash the password
+    const saltRound = 10;
+    const hash_password = await bcrypt.hash(password, saltRound);
+
+    const userCreated = await User.create({
+      username,
+      email,
+      phone: hash_password,
+    });
+    res.status(200).json({ message: userCreated });
+  } catch (error) {
+    res.status(400).send("this is error ");
+  }
+};
+module.exports = { home, register };
