34d41414756bdbb85e1347fb4db8dd2122f13c69
Nischal <tamangnischal2018@gmail.com>
2024-08-19 13:11:58 +0545

    json web token

--- server/models/user-model.js
+++ server/models/user-model.js
@@ -38,6 +38,10 @@ userSchema.pre("save", async function (next) {
   }
 });
 
+
+//json web token
+ 
+
 //define the model or the collection name
 const User = new mongoose.model("User", userSchema);
 module.exports = User;
