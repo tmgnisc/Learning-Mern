cb7eb18d6067adec476d5432faaa14668c96dff1
Nischal <tamangnischal2018@gmail.com>
2024-08-20 18:39:49 +0545

    creating an object schema

--- server/validators/auth-validator.js
+++ server/validators/auth-validator.js
@@ -1,2 +1,9 @@
-const {z} = require('zod')
+const { z } = require("zod");
 //creating an object schema
+const signupSchema = z.object({
+  username: z
+    .string({ required_error: "Name is required" })
+    .trim()
+    .min(3, { message: "Name must be more than 3 characters" })
+    .max(255, { message: "Name must be less than 255 characters" }),
+});
