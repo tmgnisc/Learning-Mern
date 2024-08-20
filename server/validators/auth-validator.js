f42d7e1290fd30ec973b8829005cc74dc0d8e1d4
Nischal <tamangnischal2018@gmail.com>
2024-08-20 18:45:12 +0545

    creating an object schema

--- server/validators/auth-validator.js
+++ server/validators/auth-validator.js
@@ -6,4 +6,13 @@ const signupSchema = z.object({
     .trim()
     .min(3, { message: "Name must be more than 3 characters" })
     .max(255, { message: "Name must be less than 255 characters" }),
+  email: z
+    .string({ required_error: "email is required" })
+    .trim()
+    .email({ message: "Invalid email address" })
+    .min(3, { message: "Email must be more than 3 characters" })
+    .max(255, { message: "email should not be more than 255 characters" }),
+    phone: z 
+    .string({})
+
 });
