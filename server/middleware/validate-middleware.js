d52525987bf76281930491583814b5cb14d0c011
Nischal <tamangnischal2018@gmail.com>
2024-08-21 18:43:46 +0545

    handling zod errors in the response

--- server/middleware/validate-middleware.js
+++ server/middleware/validate-middleware.js
@@ -3,8 +3,11 @@ const validate = (schema) => async (req, res, next) => {
         const pareseBody = await schema.parseAsync(req.body)
         req.body = pareseBody
         next()
-    } catch (error) {
-        res.status(400).json({msg:error})
+    } catch (err) {
+        
+        const message = err.errors.message 
+        console.log(message)
+        res.status(400).json({msg: message})
     }
 } 
 module.exports = validate
