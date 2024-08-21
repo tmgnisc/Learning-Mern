2c857026bbeedab2c617749aa735d37168dcc854
Nischal <tamangnischal2018@gmail.com>
2024-08-21 18:56:13 +0545

    coding the error middleware

--- server/middleware/validate-middleware.js
+++ server/middleware/validate-middleware.js
@@ -4,10 +4,16 @@ const validate = (schema) => async (req, res, next) => {
         req.body = pareseBody
         next()
     } catch (err) {
-        
+        const status = 422
         const message = err.errors[0].message 
-        //console.log(message)
+
+        const error = {
+            status,
+            message, 
+        }
+        console.log(error)
         res.status(400).json({msg: message})
+        next(error)
     }
 } 
 module.exports = validate
