5dce4ae22b63afc8e5cfe8e9c54ac12b272e28c9
Nischal <tamangnischal2018@gmail.com>
2024-08-28 19:51:38 +0545

    building contact form logic in controllers file

--- server/controllers/contact-controller.js
+++ server/controllers/contact-controller.js
@@ -1,11 +1,13 @@
 const Contact = require("../models/contact-model")
 
-const contactForm = () =>{
+const contactForm = async(req, res) =>{
     try {
 
 
-        const responise
-        
+        const response = req.body
+
+        await Contact.create(response)
+        return res.status(200).json({message: "message is sent successfully"})
     } catch (error) {
         
     }
