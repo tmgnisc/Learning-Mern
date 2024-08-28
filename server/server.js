de7cf30ad6563618ba36196ddd4c2e34c4913dab
Nischal <tamangnischal2018@gmail.com>
2024-08-28 19:54:39 +0545

    server.js adding route

--- server/server.js
+++ server/server.js
@@ -2,11 +2,13 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
-const router = require("./router/auth-router");
+const authRoute = require("./router/auth-router");
+const contactRoute = require("./router/contact-router")
 const connectDb = require("./utils/db");
 const errorMiddleware = require("./middleware/error-middleware");
 app.use(express.json());
-app.use("/api/auth", router);
+app.use("/api/auth", authRoute);
+app.use("/api/form", contactRoute)
 app.use(errorMiddleware);
 const port = 5000;
 connectDb().then(() => {
