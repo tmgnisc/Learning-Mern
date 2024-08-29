c39cea6f422406f9828fdcdcd7f81cdda42af44f
Nischal <tamangnischal2018@gmail.com>
2024-08-29 17:00:08 +0545

    added route in app jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./Pages/Home";
+import Services from "./Pages/Services";
+import Login from "./Pages/Login";
+import Register from "./Pages/Register";
+import Contact from "./Pages/Contact";
 import About from "./Pages/About";
 const App = () => {
   return (
@@ -8,6 +12,11 @@ const App = () => {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/about" element={<About/>} />
+          <Route path="/service" element={<Services/>} />
+          <Route path="/login" element={<Login/>}/>
+          <Route path="/register" element={<Register/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+      
         </Routes>
       </BrowserRouter>
     </>
