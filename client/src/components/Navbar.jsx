25df5dc518871d61c73bb7930335dc9efe12b528
Nischal <tamangnischal2018@gmail.com>
2024-08-31 18:30:59 +0545

    creating nav bar using core css

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,25 +1,19 @@
 // NavBar.js
 import React from 'react';
-import './index.css'
+import '../index.css'
 const NavBar = () => {
   return (
     <div className="navbar">
-      <a href="#home" className="active">Home</a>
-      <a href="#news">News</a>
-      <a href="#contact">Contact</a>
-      <div className="dropdown">
-        <button className="dropbtn">
-          Dropdown 
-          <i className="fa fa-caret-down"></i>
-        </button>
-        <div className="dropdown-content">
-          <a href="#link1">Link 1</a>
-          <a href="#link2">Link 2</a>
-          <a href="#link3">Link 3</a>
-        </div>
-      </div> 
+    <a href="/">Home</a>
+    <a href="/about">About Us</a>
+    <a href="/contact">Contact</a>
+    <div className="navbar-right">
+      <a href="/login">Login</a>
+      <a href="/register">Register</a>
     </div>
-  );
+  </div>
+);
 };
+
 
 export default NavBar;
