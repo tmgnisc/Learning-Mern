3ae26b034162c02197a994e9c9a6b7bc79d88403
Nischal <tamangnischal2018@gmail.com>
2024-09-01 17:29:52 +0545

    completed

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,18 @@
 import { NavLink } from "react-router-dom";
-import React from 'react';
-import '../index.css'
+import React from "react";
+import "../index.css";
 const NavBar = () => {
   return (
     <div className="navbar">
-    <NavLink to="/">Home</NavLink>
-    <NavLink to="/about">About Us</NavLink>
-    <NavLink to="/contact">Contact</NavLink>
-    <div className="navbar-right">
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/register">Register</NavLink>
+      <NavLink to="/">Home</NavLink>
+      <NavLink to="/about">About Us</NavLink>
+      <NavLink to="/contact">Contact</NavLink>
+      <div className="navbar-right">
+        <NavLink to="/login">Login</NavLink>
+        <NavLink to="/register">Register</NavLink>
+      </div>
     </div>
-  </div>
-);
+  );
 };
-
 
 export default NavBar;
