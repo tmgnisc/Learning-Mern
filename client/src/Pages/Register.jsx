cc2315e7f9d64081c4bbcba3a7cf72155a13c008
Nischal <tamangnischal2018@gmail.com>
2024-09-01 17:48:04 +0545

    creating registration form

--- client/src/Pages/Register.jsx
+++ client/src/Pages/Register.jsx
@@ -3,8 +3,20 @@ import React, { useState } from 'react';
 const Register = () =>{
  const [formValues, setFormValues] = useState({
     username: '',
-    
+    email: '',
+    phone: '',
+    password:''
 
  })
+
+ //handle input change
+
+ const handleChange = (e) => {
+    const {name, value} = e.target
+    setFormValues({
+        ...formValues, [name] : value 
+        
+    })
+ }
 }
 export default Register
