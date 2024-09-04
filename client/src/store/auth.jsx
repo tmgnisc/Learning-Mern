f33b553cb1764575c9f2517e7f8018fdea3a7146
Nischal <tamangnischal2018@gmail.com>
2024-09-04 18:11:36 +0545

    utilitzing the useContext hook for consumer functionality

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -14,5 +14,6 @@ export const AuthProvider = ({ children }) => {
 };
 
 export const useAuth = () =>{
-    return useContext(AuthContext)
+    const authContextValue = useContext(AuthContext)
+    return 
 }
