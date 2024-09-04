c754def5924fc612040ec1e85314fd7ecc729fc0
Nischal <tamangnischal2018@gmail.com>
2024-09-04 18:40:46 +0545

    creating a variable to check token status

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,16 +1,24 @@
 import { NavLink } from "react-router-dom";
 import React from "react";
 import "../index.css";
+import { useAuth } from "../store/auth";
 const NavBar = () => {
+  const { isLoggedIn } = useAuth();
   return (
     <div className="navbar">
       <NavLink to="/">Home</NavLink>
       <NavLink to="/about">About Us</NavLink>
       <NavLink to="/contact">Contact</NavLink>
       <div className="navbar-right">
-        <NavLink to="/login">Login</NavLink>
-        <NavLink to="/logout">LogOut</NavLink>
-        <NavLink to="/register">Register</NavLink>
+        {isLoggedIn ? (
+          <NavLink to="/logout">LogOut</NavLink>
+        ) : (
+          <>
+            {" "}
+            <NavLink to="/login">Login</NavLink>
+            <NavLink to="/register">Register</NavLink>
+          </>
+        )}
       </div>
     </div>
   );
