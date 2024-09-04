ca357aaf02d5e1a954967f44b68cecbc04729b22
Nischal <tamangnischal2018@gmail.com>
2024-09-04 18:00:34 +0545

    implementing context api

--- client/src/store/auth.jsx
+++ client/src/store/auth.jsx
@@ -1,1 +1,13 @@
 import { createContext } from "react";
+
+
+
+export const AuthContext = createContext()
+
+export const AuthProvider = ({children}) => {
+    return <AuthContext.Provider value={}> 
+{children}
+
+
+    </AuthContext.Provider>
+}
