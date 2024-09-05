ec68d5f13aa06be12a2ef1923daf5cd8c7440fd6
Nischal <tamangnischal2018@gmail.com>
2024-09-05 16:27:15 +0545

    implementation of authMiddleware to verify jwt

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -1,6 +1,12 @@
 const jwt = require("jsonwebtoken")
 const authMiddleware = async (req, res, next) =>{
+const token = req.header("Authorization")
 
+if(!token){
+    return res,status(401).json({msg:"token not provided"})
+}
+console.log(`token from auth middleware ${token}`)
+next()
 }
 
 module.exports = authMiddleware
