211646fcbbe317fa4b18ad88224b07a50b7d0279
Nischal <tamangnischal2018@gmail.com>
2024-09-05 16:17:25 +0545

    implementation of authMiddleware to verify jwt

---
+++ server/middleware/auth-middleware.js
@@ -0,0 +1,6 @@
+const jwt = require("jsonwebtoken")
+const authMiddleware = (req, res, next) =>{
+
+}
+
+module.exports = authMiddleware
--- server/router/auth-router.js
+++ server/router/auth-router.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const authControllers = require("../controllers/auth-controller");
+const authMiddleware = require("../middleware/auth-middleware")
 const {signupSchema, loginSchema} = require("../validators/auth-validator")
 
 const validate = require("../middleware/validate-middleware")
