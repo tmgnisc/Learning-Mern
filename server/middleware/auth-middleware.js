9dd47c1ef86849c581d181d713d792e1bb079dc5
Nischal <tamangnischal2018@gmail.com>
2024-09-05 16:45:20 +0545

    implementation of authMiddleware to verify jwt

--- server/middleware/auth-middleware.js
+++ server/middleware/auth-middleware.js
@@ -5,11 +5,19 @@ const authMiddleware = async (req, res, next) => {
   if (!token) {
     return res, status(401).json({ msg: "token not provided" });
   }
- // console.log(`token from auth middleware ${token}`);
+  // console.log(`token from auth middleware ${token}`);
 
   const jwtToken = token.replace("Bearer", "").trim();
-  console.log("this is token from middleware", jwtToken)
-  next();
+  console.log("this is token from middleware", jwtToken);
+  try {
+    const isVerified = jwtToken.verify(jwtToken, process.env.JWT_SECRET_KEY);
+    console.log("this is verified data", isVerified);
+    
+
+    next();
+  } catch (error) {
+    return res.status(401), json({ msg: "token is invalid" });
+  }
 };
 
 module.exports = authMiddleware;
