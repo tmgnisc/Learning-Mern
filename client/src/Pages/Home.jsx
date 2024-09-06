55bcc3acf3c560453ea9cff26e694e4d6acd0a5e
Nischal <tamangnischal2018@gmail.com>
2024-09-06 17:41:45 +0545

    showing username from db

--- client/src/Pages/Home.jsx
+++ client/src/Pages/Home.jsx
@@ -2,6 +2,17 @@ import React from 'react'
 import { useAuth } from '../store/auth'
 
 const Home = () =>{
-  
+  const {user, isLoggedin} = useAuth() 
+  return(
+
+    <div>
+        <h1>this is home page</h1>
+        {isLoggedin && user ? (
+            <h2>Welcome, {user.username}!</h2>
+        ) : (
+            <p>please login first</p>
+        )}
+    </div>
+  )
 }
 export default Home
