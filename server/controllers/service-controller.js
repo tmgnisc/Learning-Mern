b4a1fa9fd03347093842343c1a0f1fac721ef228
Nischal <tamangnischal2018@gmail.com>
2024-09-10 17:50:22 +0545

    crafting controllers fro service logic

--- server/controllers/service-controller.js
+++ server/controllers/service-controller.js
@@ -1,10 +1,11 @@
-const ServiceDetails = require("../models/service-model")
-const services = async (req, res) =>{
-    try {
-        const response = await ServiceDetails.find()
-        if(!response)
-        res.status(200).json({msg: response})
-    } catch (error) {
-        return res.status(500).json({msg:"this is error service is interrupted due to", error})
-    }
-}
+const ServiceDetails = require("../models/service-model");
+const services = async (req, res) => {
+  try {
+    const response = await ServiceDetails.find();
+    if (!response) res.status(200).json({ msg: response });
+  } catch (error) {
+    return res
+      .status(500)
+      .json({ msg: "this is error service is interrupted due to", error });
+  }
+};
