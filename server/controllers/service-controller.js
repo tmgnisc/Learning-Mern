faae1bf22161e6f86b64495c87be723b8a7bb21e
Nischal <tamangnischal2018@gmail.com>
2024-09-10 18:15:15 +0545

    finally bug solved

--- server/controllers/service-controller.js
+++ server/controllers/service-controller.js
@@ -1,12 +1,22 @@
 const ServiceDetails = require("../models/service-model");
+
 const services = async (req, res) => {
   try {
     const response = await ServiceDetails.find();
-    if (!response) res.status(200).json({ msg: response });
+
+    // Check if the response is empty
+    if (response.length === 0) {
+      return res.status(404).json({ msg: "No services found" });
+    }
+
+    // If data exists, return the response
+    res.status(200).json(response);
+
   } catch (error) {
     return res
       .status(500)
-      .json({ msg: "this is error service is interrupted due to", error });
+      .json({ msg: "Service is interrupted due to an error", error });
   }
 };
-module.exports = services
+
+module.exports = services;
