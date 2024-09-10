f839cf68cb41ad03e80da7d6e7864b775abc8036
Nischal <tamangnischal2018@gmail.com>
2024-09-10 18:15:36 +0545

    finally bug solved

--- server/controllers/service-controller.js
+++ server/controllers/service-controller.js
@@ -4,14 +4,11 @@ const services = async (req, res) => {
   try {
     const response = await ServiceDetails.find();
 
-    // Check if the response is empty
     if (response.length === 0) {
       return res.status(404).json({ msg: "No services found" });
     }
 
-    // If data exists, return the response
     res.status(200).json(response);
-
   } catch (error) {
     return res
       .status(500)
