05ce7b797633b7ccca4b55180a5d6673111482ed
Nischal <tamangnischal2018@gmail.com>
2024-09-12 18:11:54 +0545

    fetching data from API

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -5,6 +5,12 @@ const Services = () => {
     const [services, setServices] = useState([])
     useEffect(() =>{
         //fetch data from API
+        const fetchServices = async () =>{
+            try{
+                const response = fetch(`http://localhost:5000/api/data/service`)
+                const data = await response.json()
+            }
+        }
     })
     return (
         <h1>this is service page</h1>
