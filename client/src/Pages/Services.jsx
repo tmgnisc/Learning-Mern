c55138b9617f0ff196b4e8cb527560b45c1bbf4f
Nischal <tamangnischal2018@gmail.com>
2024-09-12 18:10:45 +0545

    fetching data from API

--- client/src/Pages/Services.jsx
+++ client/src/Pages/Services.jsx
@@ -2,7 +2,10 @@ import React, {useEffect, useState} from "react"
 
 const Services = () => {
 
-    
+    const [services, setServices] = useState([])
+    useEffect(() =>{
+        //fetch data from API
+    })
     return (
         <h1>this is service page</h1>
     )
