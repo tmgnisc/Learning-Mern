e706bbc4beabd1045506d38083064647b20a96e2
Nischal <tamangnischal2018@gmail.com>
2024-09-18 17:41:30 +0545

    sending error to frontend

--- server/middleware/validate-middleware.js
+++ server/middleware/validate-middleware.js
@@ -14,6 +14,7 @@ const validate = (schema) => async (req, res, next) => {
             extraDetails,
         }
         console.log(error)
+        res.status(400).json(error)
         //res.status(400).json({msg: message})
         next(error)
     }
