92782ec57e74dcf79be58b2083fa81c01ddbdbc1
Nischal <tamangnischal2018@gmail.com>
2024-10-04 18:52:24 +0545

    adding router to main server.js file

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -4,6 +4,8 @@ const getAllUsers = async (req, res) => {
   try {
 
     const users = await User.find()
+    console.log(users);
+    
     if(!users || users.length === 0){
         return res.status(404).json({message: "no users found"})
     }
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const getAllUsers = require('../controllers/admin-controller')
 const router = express.Router()
 
 router.route('/users').get(getAllUsers)
