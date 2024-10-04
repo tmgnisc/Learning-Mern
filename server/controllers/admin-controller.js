4d8036054208445078bd3231c250f7a0c379bbf6
Nischal <tamangnischal2018@gmail.com>
2024-10-04 18:43:25 +0545

    controller complte

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -4,7 +4,13 @@ const getAllUsers = async (req, res) => {
   try {
 
     const users = await User.find()
+    if(!users || users.length === 0){
+        return res.status(404).json({message: "no users found"})
+    }
+    res.status(200).json(users)
   } catch (error) {
     next(error);
   }
 };
+
+module.exports = getAllUsers
