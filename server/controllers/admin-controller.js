4aab326e4001680ab72eb6f459932849bac5ba60
Nischal <tamangnischal2018@gmail.com>
2024-10-04 18:40:30 +0545

    admin user controller for fetching user data

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,3 +1,10 @@
-const getAllUsers = () =>{
-    
-}
+const User = require("../models/user-model");
+
+const getAllUsers = async (req, res) => {
+  try {
+
+    const users = await User.find()
+  } catch (error) {
+    next(error);
+  }
+};
