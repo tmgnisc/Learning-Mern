0f5d4773732f7da8f005f880c59a1c41fccd1a05
Nischal <tamangnischal2018@gmail.com>
2024-10-05 11:46:04 +0545

    making contact get api for admin panel

--- server/controllers/admin-controller.js
+++ server/controllers/admin-controller.js
@@ -1,9 +1,11 @@
 const User = require("../models/user-model");
+const Contacts = require("../models/contact-model")
 
-const getAllUsers = async (req, res) => {
+const getAllUsers = async (req, res, next) => {
   try {
 
-    const users = await User.find()
+    const users = await User.find().select("-password");
+
     console.log(users);
     
     if(!users || users.length === 0){
@@ -14,5 +16,14 @@ const getAllUsers = async (req, res) => {
     next(error);
   }
 };
+
+
+const getAllContacts = async (req, res, next) =>{
+  try {
+    const contacts = await Contacts.find()
+  } catch (error) {
+    next(error)
+  }
+}
 
 module.exports = getAllUsers
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -3,5 +3,6 @@ const getAllUsers = require('../controllers/admin-controller')
 const router = express.Router()
 
 router.route('/users').get(getAllUsers)
+router.route('/contacts').get(getAllContacts)
 
 module.exports = router
