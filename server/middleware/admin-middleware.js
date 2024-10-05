b1b5d3f9a074d5d12bbfd0b17808145584d5e7ff
Nischal <tamangnischal2018@gmail.com>
2024-10-05 18:09:21 +0545

    checking isadmin or not

--- server/middleware/admin-middleware.js
+++ server/middleware/admin-middleware.js
@@ -1,7 +1,11 @@
 const adminMiddleware = async (req, res, next) => {
   try {
+    console.log(req.user);
+    res.status(200).json({msg: req.user })
     
   } catch (error) {
     next(error);
   }
 };
+
+module.exports = adminMiddleware
--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const adminController = require('../controllers/admin-controller')
 const authMiddleware = require('../middleware/auth-middleware')
+const adminMiddleware = require('../middleware/admin-middleware')
 
 const router = express.Router()
 
-router.route('/users').get(authMiddleware, adminController.getAllUsers)
+router.route('/users').get(authMiddleware, adminMiddleware, adminController.getAllUsers)
 router.route('/contacts').get(authMiddleware, adminController.getAllContacts)
 
 module.exports = router
