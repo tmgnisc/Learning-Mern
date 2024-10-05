32679a7c7e02b8f6620840eeb46cda8425e5894d
Nischal <tamangnischal2018@gmail.com>
2024-10-05 17:37:29 +0545

    securing admin route with jwt token

--- server/router/admin-router.js
+++ server/router/admin-router.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const adminController = require('../controllers/admin-controller')
+const authMiddleware = require('../middleware/auth-middleware')
 
 const router = express.Router()
 
-router.route('/users').get(adminController.getAllUsers)
+router.route('/users').get(authMiddleware, adminController.getAllUsers)
 router.route('/contacts').get(adminController.getAllContacts)
 
 module.exports = router
