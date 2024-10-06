7fb36ace3f917942235c0d5b14c793a5b4ce09e6
Nischal <tamangnischal2018@gmail.com>
2024-10-06 19:03:01 +0545

    crafting the admin navbar and outlet

--- client/src/components/layouts/Admin-Layout.jsx
+++ client/src/components/layouts/Admin-Layout.jsx
@@ -1,5 +1,21 @@
+import React from 'react';
+import {NavLink, Outlet} from "react-router-dom"
+import './AdminLayout.css'; // Make sure to link the CSS file
+
 export const AdminLayout = () => {
     return (
-        <h1>Hello admin</h1>
+        <div className="admin-layout">
+            <aside className="sidebar">
+                <h2>Admin Panel</h2>
+                <nav>
+                    <ul>
+                        <li> <NavLink to="/admin/users">Users</NavLink> </li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#contacts">Contacts</a></li>
+                    </ul>
+                </nav>
+            </aside>
+           <Outlet/>
+        </div>
     );
 };
