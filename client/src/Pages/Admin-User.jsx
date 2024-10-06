0384b50b3d475327b34ae4d757fe5bed97178ef9
Nischal <tamangnischal2018@gmail.com>
2024-10-06 19:17:44 +0545

    invoke the user route to retrieve all data

--- client/src/Pages/Admin-User.jsx
+++ client/src/Pages/Admin-User.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react"
+
 export const AdminUsers = () =>{
+
+    const getAllUsersData = async () =>{
+        try {
+            const response = await fetch(`http://localhost:5000/api/admin/users`)
+        } catch (error) {
+          console.log("error while fetching users data", error);
+            
+        }
+    }
+    useEffect(()=> {
+        getAllUsersData()
+     }, [])
+
     return(
+
+
         <h1>This is user details page</h1>
     )
 }
