be4538e69d05b28ee2ed730d0b42275331141dd5
Nischal <tamangnischal2018@gmail.com>
2024-10-06 18:39:51 +0545

    created admin layout jsx

--- server/middleware/admin-middleware.js
+++ server/middleware/admin-middleware.js
@@ -1,18 +1,19 @@
 const adminMiddleware = async (req, res, next) => {
   try {
     // console.log(req.user);
-    const adminRole = req.user.isAdmin 
-    if(!adminRole){
-        return res.status(403).json({message: "Access Denied. User is not admin."})
-     }
+    const adminRole = req.user.isAdmin;
+    if (!adminRole) {
+      return res
+        .status(403)
+        .json({ message: "Access Denied. User is not admin." });
+    }
     // res.status(200).json({msg: req.user.isAdmin})
 
     //if user is an admin, proceed to the next middleware
-    next()
-    
+    next();
   } catch (error) {
     next(error);
   }
 };
 
-module.exports = adminMiddleware
+module.exports = adminMiddleware;
